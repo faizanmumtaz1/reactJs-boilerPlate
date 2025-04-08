2ce12dc543b15e8351eca51af6d08694853e14f9
mahamtariqtkxel <maham.tariq@tkxel.io>
2025-04-08 13:04:50 +0500

    map icon updated

--- src/features/MapContainer/Map/index.tsx
+++ src/features/MapContainer/Map/index.tsx
@@ -1,12 +1,14 @@
 // components/MapView.tsx
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, DivIcon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import DefectListingTooltip from "../DefectListingTooltip/DefectListingTooltip";
+import ReactDOMServer from "react-dom/server";
+import { RedMarkerIcon } from "../../../assets/Images/svg";
 
 // Fix marker icons not showing correctly in some builds
 delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -14,6 +16,14 @@ L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
+});
+
+const redMarkerIcon = new DivIcon({
+  html: ReactDOMServer.renderToString(<RedMarkerIcon />),
+  className: "", // removes default leaflet styles
+  iconSize: [63, 63],
+  iconAnchor: [31.5, 63], // adjust if needed to align point
+  popupAnchor: [0, -63],
 });
 
 const position: LatLngExpression = [51.505, -0.09]; // Default coordinates
@@ -35,7 +45,7 @@ const MapView = ({ style = {} }: { style?: object }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={redMarkerIcon}>
         <Popup>
           <DefectListingTooltip />
         </Popup>
