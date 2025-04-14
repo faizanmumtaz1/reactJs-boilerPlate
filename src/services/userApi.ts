084926c0b1c7d92f6f3cd8d92c5774b8534c1cd1
TK-LPT-982 <tk-lpt-982@TK-LPT-0982.local>
2025-04-14 19:48:54 +0500

    server change api

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -2,7 +2,7 @@ import { BaseAPIService } from "./baseApi.service";
 
 class CustomerAPI extends BaseAPIService {
     login = (data: any) => {
-        return this.post("/v3/login", data);
+        return this.post("/login", data);
     };
 
 }
